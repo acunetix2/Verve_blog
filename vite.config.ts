826496ea9af2b1a66f6ca12fa203072e8c0bf0f9
phantomjac <drchesireyang@gmail.com>
2025-10-25 16:17:34 +0300

    Feat: Verve Blog

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.md'],
+  assetsInclude: ['**/*.md'], /*always*/
 }));
